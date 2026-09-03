0c55b5c8456b71103b2902bd6ad60aad222df53a
ndhinakaran794-maker <ndhinakaran794@gmail.com>
2026-09-03 16:11:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve frontend static files
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(__dirname));
 
 // -------------------------------------------------------------
 // 1. Database Connection
